c6d47af58a2ae3b8d4ba6920ace520a1300a2ea2
MayAbdelghany <may@qutell.com>
2023-02-05 12:22:41 +0200

    updates

---
+++ src/Domain/Entities/TawalhDesignWithConfig/OtherRoomItems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tawala.Domain.Common;
+using Tawala.Domain.Entities.Settings;
+
+namespace Tawala.Domain.Entities.TawalhDesignWithConfig
+{
+    public class OtherRoomItems : AuditableEntity, IEntityBase
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Name { get; set; }
+        public string NameEn { get; set; }
+        public Room RoomId { get; set; }
+        public Room Room { get; set; }
+        //Position
+        public decimal PositionLeft { get; set; }
+        public decimal PositionRight { get; set; }
+        public decimal PositionTop { get; set; }
+        public decimal PositionBottom { get; set; }
+        //Width && Hight
+        public decimal Width { get; set; }
+        public decimal Hight { get; set; }
+        //Image
+        public Guid? ImageId { get; set; }
+        public AppAttachment Image { get; set; }
+        //------------Json Data
+        public string JsonData { get; set; }
+    }
+}
--- src/Domain/Entities/TawalhDesignWithConfig/RoomTable.cs
+++ src/Domain/Entities/TawalhDesignWithConfig/RoomTable.cs
@@ -22,17 +22,40 @@ namespace Tawala.Domain.Entities.TawalhDesignWithConfig
         public decimal PositionRight { get; set; }
         public decimal PositionTop { get; set; }
         public decimal PositionBottom { get; set; }
-        //width
+        //Width && Hight
         public decimal Width { get; set; }
         public decimal Hight { get; set; }
-        //image
+        //Image
         public Guid? DefaultImageId { get; set; }
         public AppAttachment DefaultImage { get; set; }
-        //
+        //Image list
         public virtual IList<RoomTableImages> RoomTableImages { get; set; } = new List<RoomTableImages>();
         public int TableNumber { get; set; }
         //--Table Type Id
         public Guid? TableTypeId { get; set; }
         public OptionSetItem TableType { get; set; }
+        //-----------Days------
+        public bool IsFriday { get; set; }
+        public bool IsMonday { get; set; }
+        public bool IsSaturday { get; set; }
+        public bool IsSunday { get; set; }
+        public bool IsThursday { get; set; }
+        public bool IsTuesday { get; set; }
+        public bool IsWednesday { get; set; }
+        //---Table Avilabllity
+        public TimeSpan StartAt { get; set; }
+        public TimeSpan EndAt { get; set; }
+        //For other custome model
+        public string JsonModel { get; set; }
+        //Table Setting 
+        public int NumberOfSet { get; set; }
+        public int MaxNumberOfSet { get; set; }
+        public int DefaultNumberOfSet { get; set; }
+        //-------------Price-----
+        public decimal VipPricePerSet { get; set; }
+        public decimal VipPricePerTable { get; set; }
+        public decimal StandardPricePerSet { get; set; }
+        public decimal StandardPricePerTable { get; set; }
+
     }
 }
