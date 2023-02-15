b0e9c8d5e8c98be87d7318a52d0f62a010236d67
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-02-15 16:34:32 +0200

    updates

---
+++ src/Domain/Entities/ReservationsEn/TableReservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tawala.Domain.Common;
+using Tawala.Domain.Entities.Identity;
+using Tawala.Domain.Entities.Settings.ServiceProvider;
+
+namespace Tawala.Domain.Entities.ReservationsEn
+{
+    public class TableReservation : AuditableEntity, IEntityBase
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public Guid? RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+        //Table
+        public virtual IList<TableReservationTables> TableReservationTables { get; set; } = new List<TableReservationTables>();
+    }
+}
---
+++ src/Domain/Entities/ReservationsEn/TableReservationTables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tawala.Domain.Common;
+using Tawala.Domain.Entities.TawalhDesignWithConfig;
+
+namespace Tawala.Domain.Entities.ReservationsEn
+{
+    public class TableReservationTables : AuditableEntity, IEntityBase
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public Guid? TableReservationId { get; set; }
+        public TableReservation TableReservation { get; set; }
+        public Guid? RoomTableId { get; set; }
+        public RoomTable RoomTable { get; set; }
+    }
+}
