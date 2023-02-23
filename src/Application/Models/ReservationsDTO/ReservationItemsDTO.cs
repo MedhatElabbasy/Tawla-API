d76f52ff1fb646c43ecfeb77a937ff33ab3163e3
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-02-23 13:17:57 +0200

    updaets

--- src/Application/Models/ReservationsDTO/ReservationItemsDTO.cs
+++ src/Application/Models/ReservationsDTO/ReservationItemsDTO.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tawala.Application.Common.Mappings;
+using Tawala.Domain.Entities.menu;
+using Tawala.Domain.Entities.ReservationsEn;
 
 namespace Tawala.Application.Models.ReservationsDTO
 {
-    internal class ReservationItemsDTO
+    public class ReservationItemsAddDTO : IMapFrom<ReservationItems> 
+    { 
+        public Guid? TableReservationId { get; set; } 
+        public Guid? ItemsId { get; set; } 
+        public string Notes { get; set; }
+    }
+    public class ReservationItemsUpdateDTO : IMapFrom<ReservationItems>
     {
+        public Guid Id { get; set; } 
+        public Guid? TableReservationId { get; set; } 
+        public Guid? ItemsId { get; set; } 
+        public string Notes { get; set; }
+    }
+    public class ReservationItemsResDTO : IMapFrom<ReservationItems>
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public Guid? TableReservationId { get; set; }
+        public TableReservation TableReservation { get; set; }
+        public Guid? ItemsId { get; set; }
+        public Items Items { get; set; }
+        public string Notes { get; set; }
     }
 }
