3c573b5072856da52de05ffac8d3269a10af2505
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-02-23 13:17:46 +0200

    updates

--- src/Domain/Entities/ReservationsEn/ReservationItems.cs
+++ src/Domain/Entities/ReservationsEn/ReservationItems.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tawala.Domain.Common;
-using Tawala.Domain.Entities.TawalhDesignWithConfig;
+using Tawala.Domain.Entities.menu;
 
 namespace Tawala.Domain.Entities.ReservationsEn
 {
-    public class TableReservationTables : AuditableEntity, IEntityBase
+    public class ReservationItems : AuditableEntity, IEntityBase
     {
         public Guid Id { get; set; }
         public bool IsDeleted { get; set; }
         public Guid? TableReservationId { get; set; }
         public TableReservation TableReservation { get; set; }
-        public Guid? RoomTableId { get; set; }
-        public RoomTable RoomTable { get; set; }
-        public int NumberOfset { get; set; }
+        public Guid? ItemsId { get; set; }
+        public Items Items { get; set; }
+        public string Notes { get; set; }
     }
 }
--- src/Domain/Entities/ReservationsEn/TableReservation.cs
+++ src/Domain/Entities/ReservationsEn/TableReservation.cs
@@ -13,11 +13,30 @@ namespace Tawala.Domain.Entities.ReservationsEn
     {
         public Guid Id { get; set; }
         public bool IsDeleted { get; set; }
-        public string AppUserId { get; set; }
-        public AppUser AppUser { get; set; }
+        public string ClientId { get; set; }
+        public AppUser Client { get; set; }
         public Guid? RestaurantId { get; set; }
         public Restaurant Restaurant { get; set; }
         //Table
         public virtual IList<TableReservationTables> TableReservationTables { get; set; } = new List<TableReservationTables>();
+        //selected Itrems
+        public virtual IList<ReservationItems> ReservationItems { get; set; } = new List<ReservationItems>();
+        //----------date
+        public DateTime ReservationDate { get; set; }
+
+        public DateTime ReservationStartAt { get; set; }
+        public DateTime ReservationStartEndAt { get; set; }
+        //--notes
+        public string Notes { get; set; }
+        //----person number
+        public int NumberOfperson { get; set; }
+        //price List
+        public decimal Totalprice { get; set; }
+        public decimal TotalDiscount { get; set; }
+        public decimal TotalAfterDiscount { get; set; }
+        public decimal TotalVat { get; set; }
+        public decimal TotalAftetrVat { get; set; }
+        public decimal FinalPrice { get; set; }
+
     }
 }
