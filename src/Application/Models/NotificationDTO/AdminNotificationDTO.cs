d4dd11dbd84c6e1309c5d02e746df244199d3ffd
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-03-16 12:40:10 +0200

    updates

--- src/Application/Models/NotificationDTO/AdminNotificationDTO.cs
+++ src/Application/Models/NotificationDTO/AdminNotificationDTO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tawala.Application.Common.DateTimeCalculation;
 using Tawala.Application.Common.Mappings;
 using Tawala.Application.Models.Common;
 using Tawala.Domain.Common;
@@ -40,5 +41,8 @@ namespace Tawala.Application.Models.NotificationDTO
         public string DescriptionEn { get; set; }
         public Guid? NotificationPhotoId { get; set; }
         public AppAttachmentResDTO NotificationPhoto { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedDateTime { get { return Created.ToString("MM dddd yyyy"); } }
+        public string CreatedSinceTime { get { return Created.ToRelativeDateString(true); } }
     }
 }
