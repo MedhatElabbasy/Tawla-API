f35170a01760d99bd93e966d1afa0c96637460e3
Amr Yasin <61648679+Amryasin@users.noreply.github.com>
2023-04-04 12:43:09 +0200

    updates

---
+++ src/Infrastructure/Migrations/20230404104247_clinoupdate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tawala.Infrastructure.Migrations
+{
+    public partial class clinoupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "RestaurantId",
+                table: "ClientNotifications",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientNotifications_RestaurantId",
+                table: "ClientNotifications",
+                column: "RestaurantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ClientNotifications_Restaurants_RestaurantId",
+                table: "ClientNotifications",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClientNotifications_Restaurants_RestaurantId",
+                table: "ClientNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClientNotifications_RestaurantId",
+                table: "ClientNotifications");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "ClientNotifications");
+        }
+    }
+}
